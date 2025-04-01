5a46683a0915c238318ccd7a644909b7910334a9
yoshizumi0809 <yoshizumi.finction@docomo.ne.jp>
2025-04-01 17:58:24 +0900

    デプロイ用に変更

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors(); // ← CORSを有効にする
-  await app.listen(process.env.PORT ?? 3001);
+  const port = process.env.PORT || 3001; // 環境変数から使用ポートを取得
+  app.enableCors({
+    origin: '*',
+    allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept',
+  });
+  console.log(`linstening on port ${port}`);
+  await app.listen(port, '0.0.0.0');
 }
+
 bootstrap();
