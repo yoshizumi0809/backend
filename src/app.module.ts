e15aa7552dd14533b0c4023f01ccae0447abcd81
yoshizumi0809 <yoshizumi.finction@docomo.ne.jp>
2025-07-17 15:47:51 +0900

    7/17

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,6 +14,7 @@ import { CloudinaryModule } from './cloudinary/cloudinary.module';
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.DB_HOST,
+      port: 5432,
       username: process.env.DB_USER,
       password: process.env.DB_PASS,
       database: process.env.DB_NAME,
